9daa05e2913e314a33f12fbcf67ce0a4ccf734aa
Shivam Agarwal <shivam.mcs17.du@gmail.com>
2018-10-31 23:58:42 +0530

    Update urls.py

--- boards/urls.py
+++ boards/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='board_topics'),
+    #path('',views.home,name='board_topics')
+    path('',views.home.as_view()),name='board_topics'),
 ]
