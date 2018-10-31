6f537feaae91c4b307f8579076c8b1e5d9d8f44d
shivam2146 <shivam.mcs17.du@gmail.com>
2018-10-31 22:46:13 +0530

    update urls.py to include url of boards app into main app

--- boards/urls.py
+++ boards/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from boards import views
+from . import views
 
 urlpatterns = [
-    path('/boards',views.board_topics,name='board_topics'),
+    path('',views.home,name='board_topics'),
 ]
